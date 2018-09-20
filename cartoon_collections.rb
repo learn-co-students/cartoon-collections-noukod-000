48de4ee727dcc188bec4846cd24ee76d47e5db61
Mac Dowell Prosper <macdowell@noukod.com>
2018-09-20 17:28:20 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)# code an argument here
+  arr.each.with_index(1) {|val, index| puts "#{index}. #{val}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map {|val| val.capitalize << "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  arr.any? {|val| val.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  arr.find {|val| cheese_types.include?(val)}
 end
