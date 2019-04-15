9d5d143440208590b6d9d37af85625c88c7df23e
Ricardo Paul <kingupper0@gmail.com>
2019-04-15 17:54:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = []
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index{ |x, i| puts "#{i+1}.#{x}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (planeteer)
+  planeteer.map{ |x| x.capitalize+"!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+ calls.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(food)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  # First I iterate through the food elements
+  # for each element I check if it is included in cheese_types
+  food.find { |ingredient| cheese_types.include?(ingredient) }
 end
